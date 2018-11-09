03970d2d3506706c4e35b95c972cc0ea17d56a80
Harald Bögeholz <bo@ct.de>
2018-11-09 19:36:36 +1100

    documentation. toFree default implementation.

--- CatalanMagma.hsproj/CatalanMagma/Main.hs
+++ CatalanMagma.hsproj/CatalanMagma/Main.hs
@@ -1,18 +1,34 @@
 -- Catalan families as Magmas
 -- by Harald Bögeholz (harald.boegeholz@monash.edu)
 
+{-
+This code implements some of the ideas of the following paper [1]:
+@article{brak2018universal,
+  title={A Universal Bijection for Catalan Structures},
+  author={Brak, Richard},
+  journal={arXiv preprint arXiv:1808.09078},
+  year={2018}
+}
+-}
+
 data FreeMagma = Gen | FreeMagma :*: FreeMagma deriving (Show, Eq)
 
-class CatalanMagma a where
+-- | a unique factorisation normed magma with one irreducible element
+class (Eq a) => CatalanMagma a where
 
+  -- | the unique irreducible element
   generator :: a
 
+  -- | magma product map
   (.*.) :: a -> a -> a
 
+  -- | full multiplication
   fromFree :: FreeMagma -> a
   fromFree Gen = generator
   fromFree (a :*: b) = fromFree a .*. fromFree b
   
+  -- | enumerate all elements with given norm. Could be more efficient if
+  -- it used more space to remember all elements with smaller norm.
   enumerate :: Int -> [a]
   enumerate n
     | n <= 1 = [generator]
@@ -23,8 +39,17 @@ class CatalanMagma a where
                   ]
                   
   norm :: a -> Int
+  
+  -- | decomposition. The default implementation is inefficient since it
+  -- simply enumerates all elements with given norm until it finds the correct
+  -- one.
+  toFree :: a -> FreeMagma
+  toFree x = head $ [y | (x', y) <- zip (enumerate n) (enumerate n)
+                       , x' == x ]
+    where n = norm x
 
 -- --------------------------------------------------------------------------
+-- | a free magma with one generator 'Gen' and product ':*:'
 instance CatalanMagma FreeMagma where
   generator = Gen
   (.*.) = (:*:)
@@ -35,15 +60,17 @@ instance CatalanMagma FreeMagma where
     
 
 -- --------------------------------------------------------------------------
+-- | Dyck words. Matching pairs of brackets. $\F_1$ in [1].
 newtype DyckWord = DyckWord String deriving (Eq, Show)
 
 instance CatalanMagma DyckWord where
   generator = DyckWord ""
-  DyckWord a .*. DyckWord b  = DyckWord $ a ++ "(" ++ b ++ ")"
+  DyckWord a .*. DyckWord b  = DyckWord $ a ++ "{" ++ b ++ "}"
   norm (DyckWord s) = length s `div` 2 + 1
   
 
 -- --------------------------------------------------------------------------
+-- | 321-avoiding permutations. $F_9$ in [1].
 newtype PermAvoiding321 = PermAvoiding321 [Int] deriving (Eq, Show)
 
 instance CatalanMagma PermAvoiding321 where
